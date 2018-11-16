55cb908474d44a2a2bb3db74dc8e3bae013c5925
xuwei <1023822467@qq.com>
2018-11-16 16:30:01 +0800

    shiro简单整合

--- ssm_demo/src/main/java/com/nov/controller/UserController.java
+++ ssm_demo/src/main/java/com/nov/controller/UserController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
-@RequiresRoles("admin")
 public class UserController {
 	
 	@RequestMapping("add")
+	@RequiresRoles("admin")
 	public String addUser() {
 		
 		return "indexs";
