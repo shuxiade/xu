a32f0589144eeccaf933be5d18b8c9e7af061092
xuwei <1023822467@qq.com>
2018-11-16 16:54:21 +0800

    添加pagehelper

--- ssm_demo/src/main/java/com/nov/controller/UserController.java
+++ ssm_demo/src/main/java/com/nov/controller/UserController.java
@@ -1,19 +1,44 @@
 package com.nov.controller;
 
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.authz.annotation.RequiresRoles;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nov.entity.User;
+import com.nov.service.impl.UserService;
+
 
 @Controller
 @RequestMapping("/admin")
 public class UserController {
 	
-	@RequestMapping("add")
-	@RequiresRoles("admin")
-	public String addUser() {
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/select")
+	public List<User> select(){
+		List<User> users = userService.findAll();
+		return users;
+	}
+	@PostMapping("add")
+	public String add() {
 		
 		return "indexs";
 	}
 
+	@PostMapping("delete")
+	public String delete(@PathVariable int id) {
+		
+		return "";
+	}
+	
+	@PostMapping("udpate")
+	public String update(User user) {
+	
+		return "";
+	}
 }
